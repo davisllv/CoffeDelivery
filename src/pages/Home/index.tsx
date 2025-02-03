e2dad7f9979f550067949297106f6d555f2ec9fa
Davi da Silva <davisllvsnt57@gmail.com>
2025-02-03 18:40:56 -0300

    created the card container, also importet all the pictures

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,186 @@
 import CoffeImagge from "../../assets/coffe-delivery-image.svg";
+import TraditionalCoffe from "../../assets/coffes/american-coffe.svg";
 import {
+  ButtonCountDownElements,
+  CardButtonContainers,
+  CardContainer,
+  CardGrid,
+  CardImageContainer,
+  CardInputGroupContainer,
+  CardSectionContainer,
+  CardTextsContainer,
   ContainerHome,
   ImageContainer,
   MainContainerHome,
   TextsContainer,
 } from "./styles";
 import { HomeIcons } from "./Components/Icons";
+import { Minus, Plus, ShoppingCart } from "@phosphor-icons/react";
 
 export const Home = () => {
   return (
-    <ContainerHome>
-      <MainContainerHome>
-        <TextsContainer>
-          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-          <span>
-            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </span>
-        </TextsContainer>
+    <div>
+      <ContainerHome>
+        <MainContainerHome>
+          <TextsContainer>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <span>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </span>
+          </TextsContainer>
 
-        <HomeIcons />
-      </MainContainerHome>
+          <HomeIcons />
+        </MainContainerHome>
 
-      <ImageContainer>
-        <img src={CoffeImagge} alt="" />
-      </ImageContainer>
-    </ContainerHome>
+        <ImageContainer>
+          <img src={CoffeImagge} alt="" />
+        </ImageContainer>
+      </ContainerHome>
+
+      <CardSectionContainer>
+        <h2>Nossos Cafés</h2>
+
+        {/* Cards com os cafés */}
+        <CardGrid>
+          <CardContainer>
+            <CardImageContainer>
+              <img src={TraditionalCoffe} alt="" />
+              <span>Tradicional</span>
+            </CardImageContainer>
+            <CardTextsContainer>
+              <h3>Expresso Tradicional</h3>
+              <span>
+                O tradicional café feito com água quente e grãos moídos
+              </span>
+            </CardTextsContainer>
+
+            <CardButtonContainers>
+              <p>
+                <span>R$ </span>
+                9,90
+              </p>
+              <CardInputGroupContainer>
+                <ButtonCountDownElements>
+                  <button>
+                    <Minus />
+                  </button>
+
+                  <span>{1}</span>
+                  <button>
+                    <Plus />
+                  </button>
+                </ButtonCountDownElements>
+                <button type="button">
+                  <ShoppingCart size={22} weight="fill" />
+                </button>
+              </CardInputGroupContainer>
+            </CardButtonContainers>
+          </CardContainer>
+          <CardContainer>
+            <CardImageContainer>
+              <img src={TraditionalCoffe} alt="" />
+              <span>Tradicional</span>
+            </CardImageContainer>
+            <CardTextsContainer>
+              <h3>Expresso Tradicional</h3>
+              <span>
+                O tradicional café feito com água quente e grãos moídos
+              </span>
+            </CardTextsContainer>
+
+            <CardButtonContainers>
+              <p>
+                <span>R$ </span>
+                9,90
+              </p>
+              <CardInputGroupContainer>
+                <ButtonCountDownElements>
+                  <button>
+                    <Minus />
+                  </button>
+
+                  <span>{1}</span>
+                  <button>
+                    <Plus />
+                  </button>
+                </ButtonCountDownElements>
+                <button type="button">
+                  <ShoppingCart size={22} weight="fill" />
+                </button>
+              </CardInputGroupContainer>
+            </CardButtonContainers>
+          </CardContainer>
+          <CardContainer>
+            <CardImageContainer>
+              <img src={TraditionalCoffe} alt="" />
+              <span>Tradicional</span>
+            </CardImageContainer>
+            <CardTextsContainer>
+              <h3>Expresso Tradicional</h3>
+              <span>
+                O tradicional café feito com água quente e grãos moídos
+              </span>
+            </CardTextsContainer>
+
+            <CardButtonContainers>
+              <p>
+                <span>R$ </span>
+                9,90
+              </p>
+              <CardInputGroupContainer>
+                <ButtonCountDownElements>
+                  <button>
+                    <Minus />
+                  </button>
+
+                  <span>{1}</span>
+                  <button>
+                    <Plus />
+                  </button>
+                </ButtonCountDownElements>
+                <button type="button">
+                  <ShoppingCart size={22} weight="fill" />
+                </button>
+              </CardInputGroupContainer>
+            </CardButtonContainers>
+          </CardContainer>
+          <CardContainer>
+            <CardImageContainer>
+              <img src={TraditionalCoffe} alt="" />
+              <span>Tradicional</span>
+            </CardImageContainer>
+            <CardTextsContainer>
+              <h3>Expresso Tradicional</h3>
+              <span>
+                O tradicional café feito com água quente e grãos moídos
+              </span>
+            </CardTextsContainer>
+
+            <CardButtonContainers>
+              <p>
+                <span>R$ </span>
+                9,90
+              </p>
+              <CardInputGroupContainer>
+                <ButtonCountDownElements>
+                  <button>
+                    <Minus />
+                  </button>
+
+                  <span>{1}</span>
+                  <button>
+                    <Plus />
+                  </button>
+                </ButtonCountDownElements>
+                <button type="button">
+                  <ShoppingCart size={22} weight="fill" />
+                </button>
+              </CardInputGroupContainer>
+            </CardButtonContainers>
+          </CardContainer>
+        </CardGrid>
+      </CardSectionContainer>
+    </div>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -65,3 +65,144 @@ export const TextsContainer = styled.div`
 export const ImageContainer = styled(DefaultThemeBackground)`
   position: relative;
 `;
+
+export const CardSectionContainer = styled.section`
+  padding: 2rem 10rem;
+
+  h2 {
+    font-family: "Baloo 2", sans-serif;
+    font-size: 2rem;
+    font-weight: bolder;
+
+    color: ${(props) => props.theme["base-subtitle"]};
+  }
+`;
+
+export const CardGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 2rem;
+`;
+
+export const CardContainer = styled.div`
+  padding: 1.25rem;
+  margin-top: 3.5rem;
+
+  max-width: 16rem;
+
+  background: ${(props) => props.theme["base-card"]};
+  border-radius: 6px;
+
+  border-top-right-radius: 36px;
+  border-bottom-left-radius: 36px;
+`;
+
+export const CardImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.75rem;
+  margin-top: -3rem;
+  span {
+    border-radius: 100px;
+    background: ${(props) => props.theme["yellow-light"]};
+
+    font-size: 0.625rem;
+    font-weight: bold;
+
+    color: ${(props) => props.theme["yellow-dark"]};
+
+    padding: 0.25rem 0.5rem;
+  }
+`;
+
+export const CardTextsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  margin-top: 1rem;
+
+  h3 {
+    color: ${(props) => props.theme["base-subtitle"]};
+    font-family: "Baloo 2", sans-serif;
+    font-size: 1.25rem;
+    font-weight: bold;
+  }
+  span {
+    color: ${(props) => props.theme["base-label"]};
+    font-size: 0.875rem;
+  }
+`;
+
+export const CardButtonContainers = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex: 1;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 2rem;
+
+  padding: 0.25rem 0;
+
+  gap: 1.375rem;
+  color: ${(props) => props.theme["base-text"]};
+  p {
+    span {
+      font-size: 0.875rem;
+      font-weight: 400;
+    }
+    font-family: "Baloo 2", sans-serif;
+    font-weight: bolder;
+    font-size: 1.5rem;
+  }
+`;
+
+export const CardInputGroupContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 0.5rem;
+
+  button {
+    display: flex;
+    align-items: center;
+    border: 0;
+    padding: 0.5rem;
+    border-radius: 6px;
+    color: ${(props) => props.theme["base-card"]};
+    background: ${(props) => props.theme["purple-dark"]};
+  }
+`;
+
+export const ButtonCountDownElements = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  padding: 0.5rem;
+
+  border-radius: 6px;
+
+  background: ${(props) => props.theme["base-button"]};
+
+  button {
+    cursor: pointer;
+    border: 0;
+    padding: 0;
+    background: transparent;
+    color: ${(props) => props.theme.purple};
+
+    &:hover {
+      font-weight: bold;
+      color: ${(props) => props.theme["purple-dark"]};
+    }
+  }
+
+  span {
+    color: ${(props) => props.theme["base-title"]};
+  }
+`;
